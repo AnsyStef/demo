e9088577a97476610cb8054f2e92d67e11a90af0
Ansy Stef <ansystef@gmail.com>
2023-12-12 12:15:45 +0300

    1

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home),
+    path('en/', views.home_eng)
 ]
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
--- main/views.py
+++ main/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home_page.html')
+def home_eng(request):
+    return render(request, 'home_page_eng.html')
